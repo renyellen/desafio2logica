e59ebf28c9bb00a4a3ae97a53d79117d75703bb2
renyellen <reny1934@gmail.com>
2022-06-24 12:14:13 -0300

    desafio 2 com extras

---
+++ script.js
@@ -0,0 +1,34 @@
+var age =19;
+if (age >18){
+    console.log('você é de maior');
+}else{
+    console.log('Voê é de menor')
+}
+
+let humana=true;
+if (idade =>18 && humana === true){
+    console.log('você é humana(o)')
+}else{ 
+console.log("você não é humana ou maior de idade")
+}
+
+let aniversario="julho";
+if(aniversario === "janeiro"|| aniversario === "dezembro"){
+    console.log("você é capricorniano(a)")}
+    else{
+        console.log("você é de outro signo")
+    }
+
+    let nome="Renyellen";
+    if (nome[0]==="R"|| "r"){
+        console.log("seu nome começa com'R' ")}
+        else{
+            console.log("Seu nome não começa com'R' ")
+        }
+    
+        let sobrenome="Santos";
+        if(sobrenome.length>6 || nome.substring [0,1]==="e"){
+            console.log("seu sobrenome tem mais de 6 letras e ou começa com a letra 'e' ")
+        } else{
+            console.log("seu sobrenome não tem 6 letras ou começa com a letra'e' ")
+        }
